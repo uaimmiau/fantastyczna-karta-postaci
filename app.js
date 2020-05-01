9291eec2fe80cd2e2ab0b24719d19190f5f4e7d9
Uaimmiau <zoltanchivay2137@gmail.com>
2020-05-01 20:54:41 +0200

    Press x to json

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 //Lista nicków
 const listaNickow = document.getElementById('listaNickow');
-const tabNicki = [
-    {
-        nick: "Kroomek"
-    },
-    {
-        nick: "Queltas"
-    },
-    {
-        nick: "Uaimmiau"
-    },
-    {
-        nick: "Stah-Schek"
-    },
-    {
-        nick: "itd"
-    }
-];
+let tabGraczy = [];
+let tabMagia = [];
 
-listaNickow.innerHTML = tabNicki.map( name => {
-    return `<li class="elementListy"><p>${name.nick}</p></li>`;
-}).join("");
+
+tworzenieListy = () => {
+    listaNickow.innerHTML = tabGraczy.map( gracz => {
+        return `<li class="elementListy"><p>${gracz.nick}</p></li>`;
+    }).join("");
+}
+
+
+
 
 
 
@@ -61,71 +52,65 @@ pen.quadraticCurveTo(200,700,0,449.5);
 
 pen.stroke();
 
-//Rysowanie perko kropek
-const tabMagia = [
-    {
-        nazwa: "Ognista kula",
-        opis: "Skoncentrowana moc pozwala na manifestację żywiołu w formie kuli",
-        koszt: "10 zł",
-        x: "449.5",
-        y: "200",
-        r: "8"
-    },
-    {
-        nazwa: "lodowa kula",
-        opis: "Skoncentrowana moc pozwala na manifestację żywiołu w formie kuli",
-        koszt: "10 zł",
-        x: "449.5",
-        y: "300",
-        r: "8"
-    },
-    {
-        nazwa: "Plejsholder",
-        opis: "Skoncentrowana moc pozwala na manifestację żywiołu w formie kuli",
-        koszt: "10 zł",
-        x: "600",
-        y: "400",
-        r: "8"
-    },
-    {
-        nazwa: "Generyczna nazwa zeklęcia",
-        opis: "Skoncentrowana moc pozwala na manifestację żywiołu w formie kuli",
-        koszt: "10 zł",
-        x: "300",
-        y: "400",
-        r: "8"
-    }
-]
 
-var i = 0;
-for(i = 0; i < tabMagia.length; i++)
-{
-    var tpen = stol.getContext("2d");
-    tpen.beginPath();
-    tpen.arc(tabMagia[i].x,tabMagia[i].y,tabMagia[i].r,0,Math.PI*2,true);
-    tpen.fill();
+tworzenieMagii = () => {
+    //Rysowanie perko kropek
+
+    var i = 0;
+    for(i = 0; i < tabMagia.length; i++)
+    {
+        var tpen = stol.getContext("2d");
+        tpen.beginPath();
+        tpen.arc(tabMagia[i].x,tabMagia[i].y,tabMagia[i].r,0,Math.PI*2,true);
+        tpen.fill();
+    }
+
+    //Wyświetlanie się opisów magii i perków czy jak to się tam nazywa
+    const opisUm = document.getElementById('opisUm');
+    const opisNazwa = document.getElementById('opisNazwa');
+    const opisOpis = document.getElementById('opisOpis');
+    const opisKoszt = document.getElementById('opisKoszt');
+    const opisWyl = document.getElementById('opisWyl');
+
+    stol.addEventListener("click", e => {
+        for(i = 0;i < tabMagia.length;i++)
+        {
+            if((Math.pow((e.offsetX - parseInt(tabMagia[i].x)),2)) + (Math.pow((e.offsetY - parseInt(tabMagia[i].y)),2)) <= Math.pow(parseInt(tabMagia[i].r),2))
+            {
+                opisUm.style.display = "block";
+                opisNazwa.innerText = tabMagia[i].nazwa;
+                opisOpis.innerText = tabMagia[i].opis;
+                opisKoszt.innerText = `Koszt: ${tabMagia[i].koszt}`;
+            }
+        }  
+    })
+    opisWyl.addEventListener("click", e => {
+        opisUm.style.display = "none";
+    })
 }
 
 
-//Wyświetlanie się opisów magii i perków czy jak to się tam nazywa
-const opisUm = document.getElementById('opisUm');
-const opisNazwa = document.getElementById('opisNazwa');
-const opisOpis = document.getElementById('opisOpis');
-const opisKoszt = document.getElementById('opisKoszt');
-const opisWyl = document.getElementById('opisWyl');
 
-stol.addEventListener("click", e => {
-    for(i = 0;i < tabMagia.length;i++)
-    {
-        if((Math.pow((e.offsetX - parseInt(tabMagia[i].x)),2)) + (Math.pow((e.offsetY - parseInt(tabMagia[i].y)),2)) <= Math.pow(parseInt(tabMagia[i].r),2))
-        {
-            opisUm.style.display = "block";
-            opisNazwa.innerText = tabMagia[i].nazwa;
-            opisOpis.innerText = tabMagia[i].opis;
-            opisKoszt.innerText = `Koszt: ${tabMagia[i].koszt}`;
-        }
-    }  
-})
-opisWyl.addEventListener("click", e => {
-    opisUm.style.display = "none";
-})
+
+
+//Feczowanie fszyskiego
+
+fetchowanie = () => {
+    fetch("listaGraczy.json").then( res => {
+        return res.json();
+    }).then( tab => {
+        tabGraczy = tab;
+        tworzenieListy();
+    }).catch( err => {
+        console.error(err);
+    });
+    fetch("magia.json").then( res => {
+        return res.json();
+    }).then( tab => {
+        tabMagia = tab;
+        tworzenieMagii();
+    }).catch( err => {
+        console.error(err);
+    });
+}
+fetchowanie();
